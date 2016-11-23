abd5fecae409970e18765cde40c63be1db31dc76
gailla_a <gailla_a@2900cae0-73c2-4646-a36a-961e42337be9>
2016-11-23 13:46:37 +0000

    image en commentaire

--- fractale.php
+++ fractale.php
@@ -11,11 +11,11 @@ function fractale($input)
     echo "Entrez le nombre d'iteration et le degre.";
   else if (!empty($input['iterations']) && !empty($input['degre']))
   {
-    $image = imagecreatetruecolor(800, 600);
-    $white = imagecolorallocate($image, 255, 255, 255);
-    imagefilledrectangle($image, 100, 100, 700, 500, $white);
-    imagejpeg($image, 'test.jpg');
-    imagedestroy($image);
+//    $image = imagecreatetruecolor(800, 600);
+//    $white = imagecolorallocate($image, 255, 255, 255);
+//    imagefilledrectangle($image, 100, 100, 700, 500, $white);
+//    imagejpeg($image, 'test.jpg');
+//    imagedestroy($image);
     echo "done";
   }
   else
