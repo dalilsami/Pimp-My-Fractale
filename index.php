8e00fbdf9749a02849de2db2d6f452f119a2822f
gailla_a <gailla_a@2900cae0-73c2-4646-a36a-961e42337be9>
2016-11-23 12:43:52 +0000

    indentation index.php

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <meta charset="UTF-8">
     <title>Pimp My Fractale</title>
     <link rel="stylesheet" href="index.css">
-</head>
-<body>
-<div id="header">Pimp My Fractale</div>
-<form id="calcul-form" action="index.php" method="post">
-    <div class="form-title">Générez une fractale de l'ensemble de Mandelbrot</div>
-    <div id="iteration">
-        <div class="input-title">Nombre d'itération</div>
-        <input type="text" title="Nombre d'itération" placeholder="50">
-    </div>
-    <div id="degre">
-        <div class="input-title">Degré</div>
-        <input type="text" title="Degré" placeholder="2">
-    </div>
-    <input type="submit" title="Envoi" value="Générer">
-</form>
-</body>
+  </head>
+  <body>
+    <div id="header">Pimp My Fractale</div>
+    <form id="calcul-form" action="index.php" method="post">
+      <div class="form-title">Générez une fractale de l'ensemble de Mandelbrot</div>
+      <div id="iteration">
+	<div class="input-title">Nombre d'itération</div>
+	<input type="text" title="Nombre d'itération" placeholder="50">
+      </div>
+      <div id="degre">
+	<div class="input-title">Degré</div>
+	<input type="text" title="Degré" placeholder="2">
+      </div>
+      <input type="submit" title="Envoi" value="Générer">
+    </form>
+  </body>
 </html>
