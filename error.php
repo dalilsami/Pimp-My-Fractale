684d254fb985030ed82e98d393a3d8b06793934d
dalil_s <dalil_s@2900cae0-73c2-4646-a36a-961e42337be9>
2016-11-30 14:54:51 +0000

    PRESQUE FIN

--- error.php
+++ error.php
@@ -12,14 +12,14 @@ function error_mandelbrot()
             $degre = (int)$_GET['degre'];
         else
             $degre = 2;
-        if (preg_match("/[^0-9]/", $_GET['iterations']) || $iteration == '')
+        if (preg_match("/[^0-9]/", $_GET['iterations']) || $iteration == null)
             $iteration = "Le nombre d'itération n'est pas valide.<br>Entrez un nombre entier positif.<br>";
         else if ($iteration > 300 || $iteration < 1)
             $iteration = "Le nombre d'itération doit être compris entre 1 et 300.<br>";
-        /*if (preg_match("/[^0-9]/", $_GET['degre']) || $degre == '')
+        if (preg_match("/[^0-9]/", $_GET['degre']) || $degre == null)
             $degre = "Le degré n'est pas valide.<br>Entrez un nombre entier positif.<br>";
         else if ($degre > 15 || $degre < 1)
-            $degre = "Le degré doit être compris entre 1 et 15.<br>";*/
+            $degre = "Le degré doit être compris entre 1 et 15.<br>";
         return [$iteration, $degre];
     }
 }
@@ -29,16 +29,16 @@ function error_julia()
     if (!empty($_GET['x']) || $_GET['x'] == '0')
         $x = (float)str_replace(',', '.', $_GET['x']);
     else
-        $x = 50;
+        $x = 0.5;
     if (!empty($_GET['y']) || $_GET['y'] == '0')
         $y = (float)str_replace(',', '.', $_GET['y']);
     else
-        $y = 2;
-    if (!preg_match("/[+-]?[0-9]+([.,]?[0-9]+)?$/A", $_GET['x']) || $x == '')
-        $x = "La partie réelle n'est pas valide.<br>Entrez un nombre réel.<br>";
+        $y = 0.5;
+    if (!preg_match("/[+-]?[0-9]+([.,]?[0-9]+)?$/A", $_GET['x']) && $x != null && !empty($_GET['x']))
+        $x = "La partie réelle n'est pas valide.<br>Entrez un nombre réel.<br>, $x";
     else if ($x > 10 || $x < -10)
         $x = "La partie réelle doit être comprise entre -10 et 10.<br>";
-    if (!preg_match("/[+-]?[0-9]+([.,]?[0-9]+)?$/A", $_GET['y']) || $y == '')
+    if (!preg_match("/[+-]?[0-9]+([.,]?[0-9]+)?$/A", $_GET['y']) && $y != null && !empty($_GET['y']))
         $y = "La partie imaginaire n'est pas valide.<br>Entrez un nombre réel.<br>";
     else if ($y > 10 || $y < -10)
         $y = "La partie imaginaire doit être comprise entre -10 et 10.<br>";
