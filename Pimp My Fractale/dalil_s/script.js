f0907b60c8d69a2b0fb67a1d2d77c1eec782ac89
gailla_a <gailla_a@2900cae0-73c2-4646-a36a-961e42337be9>
2016-11-30 13:16:35 +0000

    ajout script

---
+++ Pimp My Fractale/dalil_s/script.js
@@ -0,0 +1,11 @@
+function hide()
+{
+    document.getElementById('float').style.display = "none";
+    document.getElementById('imaginary').style.display = "none";
+}
+
+function show()
+{
+    document.getElementById('float').style.display = "block";
+    document.getElementById('imaginary').style.display = "block";
+}
